59bdad502f341274390554809156b71f587f50f6
priyaadubey <priyadubey122003@gmail.com>
2023-12-30 00:51:40 +0530

    commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import json
+import pickle
+
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+## Load the model
+randomforestmodel=pickle.load(open('randomforestmodel.pkl','rb'))
+preprocessor=pickle.load(open('preprocessor.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=(np.array(list(data.values())).reshape(1,-1))
+    new_data=preprocessor.transform(new_data)
+    output=randomforestmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=(np.array(list(data.values())).reshape(1,-1))
+    final_input=preprocessor.transform(new_data)
+    
+    print(final_input)
+    output=randomforestmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The Credit limit prediction for the data is {}".format(output))
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
